ba319e82b1931e3394d85e37205e69022cca3da7
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-09-14 10:25:09 +0200

    elevate only if necessary

--- elevate.cmd
+++ elevate.cmd
@@ -1,1 +1,5 @@
-@"%~dp0.\bin__kliu_elevate-1.3.0-redist\bin.x86-64\elevate.exe" -c -w %*
+@net session >nul 2>nul && (
+    @%*
+) || (
+    @"%~dp0.\bin__kliu_elevate-1.3.0-redist\bin.x86-64\elevate.exe" -c -w %*
+)
